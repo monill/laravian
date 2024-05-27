f14b878332271d34b584b84e9c1d8b55ee214c76
John <juaorok@hotmail.com>
2024-05-27 12:59:18 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\InstallController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::namespace('Installer')->group(function () {
+    Route::prefix('install')->group(callback: function () {
+        Route::get('/', [InstallController::class, 'index'])->name('install');
+        Route::get('requirements', [InstallController::class, 'requirements'])->name('install.requirements');
+        Route::get('permissions', [InstallController::class, 'permissions'])->name('install.permissions');
+//        Route::get('environment/wizard')->name('install.environmentWizard');
+//        Route::post('environment/saveWizard')->name('install.environmentSaveWizard');
+//        Route::get('database')->name('install.database');
+//        Route::get('final')->name('install.final');
+    });
 });
