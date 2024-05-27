aa0c61bedcaf9572e4987da7043716ebd338a4f2
John <juaorok@hotmail.com>
2024-05-27 12:58:53 -0300

    Create index.blade.php

---
+++ resources/views/install/index.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Travian Installer</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://license.viserlab.com/external/install.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <style>
+        #hide {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+<div class="installation-section padding-bottom padding-top">
+    <div class="container">
+        <div class="installation-wrapper">
+            <div class="install-content-area">
+                <div class="install-item">
+                    <h3 class="bg-warning title text-center">Travian Installer</h3>
+                    <div class="box-item">
+                        <div class="item">
+                            <p class="info">
+                                Along with the installation/usage of this game, you shall be fully responsible for any legal results that
+                                may raised initiated by the owners of any unlicensed content you permit your copy of this game to publish.
+                            </p>
+                            <p class="info">
+                                Neither the team that created this script nor the team that customized it to create this
+                                distribution/release shall be responsible for any damage done to your computer/server system.
+                            </p>
+                            <p class="info">
+                                All code was confirmed to be running correctly by the creation team without any visible security risk they
+                                were aware of at the time the released it. Similarly, for the customization team who customized it to create
+                                this distribution/release.
+                            </p>
+                            <p class="info">Users are asked to review the code on their own accord and behalf.</p>
+                            <p class="info">
+                                Any customization to the source code are the property of each customization's author, it is up to each
+                                author's discretion whether to share it by including it in free open source distributions such as this one.
+                            </p>
+                            <p class="info"><b>You have no rights to edit copyright notices or/and claim this script as your own.</b></p>
+                            <p class="info">Last but not least, <b>enjoy</b>.</p>
+                            <p class="info">Yours sincerely, <br>PHPvian Team</p>
+                            <span class="text-warning"><i class="fas fa-exclamation-triangle"></i>  If any issue or error occurred for your modification on our code/database, we will not be responsible for that. </span>
+                        </div>
+                        <div class="item text-right">
+                            <a href="{{ route('install.requirements') }}" class="theme-button choto">I Agree, Next Step</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
