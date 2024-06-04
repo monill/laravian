b0b37db9546fcde0ff8fffbea36e9e06769ba0c7
John <juaorok@hotmail.com>
2024-06-04 12:57:07 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'clp',
+        'protect',
+        'activate',
     ];
 
     /**
