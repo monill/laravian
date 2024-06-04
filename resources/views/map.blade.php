52577d6a818d76cbc30caf19c75ba423460071c5
John <juaorok@hotmail.com>
2024-06-04 13:01:55 -0300

    Create map.blade.php

---
+++ resources/views/map.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <div id="background">
+        <div id="headerBar"></div>
+        <div id="bodyWrapper">
+
+            @include('partials.navbar')
+
+            <div id="center">
+                <a id="ingameManual" href="help">
+                    <img class="question" alt="Help" src="/assets/images/x.gif">
+                </a>
+
+                <div id="sidebarBeforeContent" class="sidebar beforeContent">
+                    @include('partials.hero-side')
+                    @include('partials.alliance')
+                    @include('partials.infomsg')
+                    @include('partials.links')
+                    <div class="clear"></div>
+                </div>
+                <div id="contentOuterContainer">
+                    @include('partials.res')
+                    <div class="contentTitle">
+                        <a id="closeContentButton" class="contentTitleButton" href="dorf1" title="CLOSE"></a>
+                        <a id="answersButton" class="contentTitleButton" href="#" title="TRAVIANANS"></a>
+                    </div>
+                    <div class="contentContainer">
+                        <center>
+                            <font color="red">MP_IMPORTANTCACHE</font>
+                        </center>
+                        @include('partials.map.mapview')
+                        <div class="clear"></div>
+                    </div>
+                    <div class="contentFooter"></div>
+                </div>
+                <div id="sidebarAfterContent" class="sidebar afterContent">
+                    @include('partials.sideinfo')
+                    @include('partials.multivillage')
+                    @include('partials.quest')
+                </div>
+                <div class="clear"></div>
+                @include('partials.footer')
+            </div>
+            <div id="ce"></div>
+        </div>
+    </div>
+
+@endsection
