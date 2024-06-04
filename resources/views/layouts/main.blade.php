0541c6d22c9a70d7bba71efa7c0b84deda5254c2
John <juaorok@hotmail.com>
2024-06-04 13:01:49 -0300

    Create main.blade.php

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Resources</title>
+
+    <link href="/css/compact.css" rel="stylesheet" type="text/css" />
+    <link href="/css/compact1.css" rel="stylesheet" type="text/css" />
+    <link href="/css/hero.css" rel="stylesheet" type="text/css" />
+    <link href="/css/chat.css" rel="stylesheet" type="text/css" />
+    <link href="/css/lang.css" rel="stylesheet" type="text/css" />
+
+    <script src="/js/crypt.js" type="text/javascript"></script>
+</head>
+<body>
+
+    @yield('content')
+
+</body>
+</html>
