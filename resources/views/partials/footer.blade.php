bcded234a9819d7364abcd993f009e2cf26af31d
John <juaorok@hotmail.com>
2024-06-04 13:02:07 -0300

    Create footer.blade.php

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,16 @@
+<div id="servertime" class="stime">
+    SERVER TIME &nbsp;
+    <span id="tp1">{{ date('H:i:s') }}</span>
+</div>
+
+<div id="footer">
+    <div id="pageLinks">
+        <a href="#">HOMEPAGE</a>
+        <a href="#">FORUM</a>
+        <a href="#">LINKS</a>
+        <a href="#">FAQ</a>
+        <a href="#">TERMS</a>
+        <a class="flink" href="#">RULES</a>
+    </div>
+    <p class="copyright">copyright {{ date('Y') }}&nbsp;</p>
+</div>
