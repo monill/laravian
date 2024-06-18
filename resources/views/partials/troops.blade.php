a1a33ab7136cdc2556326c5d43750239ec25dae0
John <juaorok@hotmail.com>
2024-06-18 14:49:11 -0300

    Update troops.blade.php

--- resources/views/partials/troops.blade.php
+++ resources/views/partials/troops.blade.php
@@ -1,61 +1,42 @@
 <div class="boxes villageList units">
-    <div class="boxes-tl"></div>
-    <div class="boxes-tr"></div>
-    <div class="boxes-tc"></div>
-    <div class="boxes-ml"></div>
-    <div class="boxes-mr"></div>
-    <div class="boxes-mc"></div>
-    <div class="boxes-bl"></div>
-    <div class="boxes-br"></div>
-    <div class="boxes-bc"></div>
     <div class="boxes-contents cf">
         <table id="troops" cellpadding="1" cellspacing="1">
             <thead>
             <tr>
                 <th colspan="3">
-                    <?php echo TR_TROOPS; ?>
+                    Army:
                 </th>
             </tr>
             </thead>
             <tbody>
-            <?php
-            function add_row_to_units(&$units, $row)
-            {
-                for ($i = 1; $i <= 50; ++$i) {
-                    if (!$row['u' . $i]) continue;
-                    if (!isset($units[$i])) $units[$i] = 0;
-                    $units[$i] += $row['u' . $i];
-                }
-                if ($row['hero']) {
-                    if (!isset($units['hero'])) $units['hero'] = 0;
-                    $units['hero'] += $row['hero'];
-                }
-            }
-
-            $units = array();
-            add_row_to_units($units, $database->getUnit($village->wid));
-            $enforcement = $database->getEnforceVillage($village->wid, 0);
-            foreach ($enforcement as $enforce) add_row_to_units($units, $enforce);
-            if (!array_sum($units)) {
-                echo "<tr><td>" . TR_NOTROOPS . "</td></tr>";
-            } else {
-                if ($units['hero'] > 0) {
-                    echo "<tr><td class=\"ico\"><a href=\"build.php?id=39\"><img class=\"unit uhero\" src=\"/assets/images/x.gif\" alt=\""
-                        . U0 . "\" title=\"" . U0
-                        . "\" /></a></td><td class=\"num\">"
-                        . $units['hero'] . "</td><td class=\"un\">" . U0 . "</td></tr>";
-                }
-                foreach ($units as $tid => $tnum) {
-                    if ($tid == 'hero') continue;
-                    $tname = $technology->unarray[$tid];
-                    echo "<tr><td class=\"ico\"><a href=\"build.php?id=39\"><img class=\"unit u"
-                        . $tid . "\" src=\"/assets/images/x.gif\" alt=\""
-                        . $tname . "\" title=\"" . $tname
-                        . "\" /></a></td><td class=\"num\">"
-                        . $tnum . "</td><td class=\"un\">" . $tname . "</td></tr>";
-                }
-            }
-            ?>
+            @forelse ($units as $tid => $tnum)
+                @if ($tid == 'hero')
+                    <tr>
+                        <td class="ico">
+{{--                            <a href="{{ route('build', ['id' => 39]) }}">--}}
+                                <img class="unit uhero" src="/assets/images/x.gif" alt="{{ __('U0') }}" title="{{ __('U0') }}" />
+                            </a>
+                        </td>
+                        <td class="num">{{ $tnum }}</td>
+                        <td class="un">{{ __('U0') }}</td>
+                    </tr>
+                @else
+                    @php
+                        $tname = $technology->unarray[$tid];
+                    @endphp
+                    <tr>
+                        <td class="ico">
+{{--                            <a href="{{ route('build', ['id' => 39]) }}">--}}
+                                <img class="unit u{{ $tid }}" src="/assets/images/x.gif" alt="{{ $tname }}" title="{{ $tname }}" />
+                            </a>
+                        </td>
+                        <td class="num">{{ $tnum }}</td>
+                        <td class="un">{{ $tname }}</td>
+                    </tr>
+                @endif
+            @empty
+                <tr><td>Any:</td></tr>
+            @endforelse
             </tbody>
         </table>
     </div>
