36b30c4fdbaccdf8656f66404ceaf7f68ffcd612
John <juaorok@hotmail.com>
2024-06-18 14:50:02 -0300

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Resources</title>
+    <title>Laravian</title>
 
-    <link href="/css/compact.css" rel="stylesheet" type="text/css" />
-    <link href="/css/compact1.css" rel="stylesheet" type="text/css" />
-    <link href="/css/hero.css" rel="stylesheet" type="text/css" />
-    <link href="/css/chat.css" rel="stylesheet" type="text/css" />
-    <link href="/css/lang.css" rel="stylesheet" type="text/css" />
+    @vite([
+        'resources/css/compact.css',
+        'resources/css/compact1.css',
+        'resources/css/chat.css',
+        'resources/css/lang.css',
+{{--        'resources/js/crypt.js',--}}
+    ])
 
-    <script src="/js/crypt.js" type="text/javascript"></script>
 </head>
-<body>
+<body class="v35 gecko chrome village1 perspectiveResources">
 
     @yield('content')
 
