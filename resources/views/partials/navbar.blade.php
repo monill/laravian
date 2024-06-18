184f0eef015f86467d5b66bf79e0e3c21feef9e8
John <juaorok@hotmail.com>
2024-06-18 14:49:32 -0300

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -50,43 +50,45 @@
 
     <div id="goldSilver">
         <div class="gold">
-            <img src="/assets/images/x.gif" alt="GOLD" title="Gold" class="gold" />
-            <span class="ajaxReplaceableGoldAmount">$session->gold</span>
+            <img class="gold" src="{{ Vite::asset('resources/images/x.gif') }}" alt="Gold">
+            <span class="ajaxReplaceableGoldAmount">{{ auth()->user()->gold }}</span>
         </div>
         <div class="silver">
-            <a href="hero/auction"><img src="/assets/images/x.gif" alt="SILVER" title="Silver" class="silver" /></a>
-            <span class="ajaxReplaceableSilverAmount">$session->silver</span>
+            <a class="silver" href="hero/auction">
+                <img src="{{ Vite::asset('resources/images/x.gif') }}" alt="Siler">
+            </a>
+            <span class="ajaxReplaceableSilverAmount">{{ auth()->user()->silver }}</span>
         </div>
     </div>
-    <ul id="outOfGame" class="LTR">
+    <ul id="outOfGame">
         <li class="profile">
-            <a href="spieler?uid=$session->uid" title="Profile">
-                <img src="/assets/images/x.gif" alt="Profile" />
+            <a href="spieler?uid=$session->uid">
+                <img src="{{ Vite::asset('resources/images/x.gif') }}" alt="Profile" />
             </a>
         </li>
         <li class="options">
             <a href="options" title="Settings">
-                <img src="/assets/images/x.gif" alt="Settings" />
+                <img src="{{ Vite::asset('resources/images/x.gif') }}" alt="Settings" />
             </a>
         </li>
         <li class="forum">
             <a href="#" title="Forum">
-                <img src="/assets/images/x.gif" alt="Forum" />
+                <img src="{{ Vite::asset('resources/images/x.gif') }}" alt="Forum" />
             </a>
         </li>
         <li class="chat">
             <a href="#" title="Chat">
-                <img src="/assets/images/x.gif" alt="Chat" />
+                <img src="{{ Vite::asset('resources/images/x.gif') }}" alt="Chat" />
             </a>
         </li>
         <li class="help">
             <a href="help" title="Help">
-                <img src="/assets/images/x.gif" alt="Help" />
+                <img src="{{ Vite::asset('resources/images/x.gif') }}" alt="Help" />
             </a>
         </li>
         <li class="logout">
             <a href="logout" title="Logout">
-                <img src="/assets/images/x.gif" alt="Logout" />
+                <img src="{{ Vite::asset('resources/images/x.gif') }}" alt="Logout" />
             </a>
         </li>
         <li class="clear"></li>
