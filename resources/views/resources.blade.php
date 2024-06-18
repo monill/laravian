ed74b43a37e41befdbc57cd23d0366dcb04f0010
John <juaorok@hotmail.com>
2024-06-18 14:49:01 -0300

    Update resources.blade.php

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -10,142 +10,70 @@
 
             <div id="center">
                 <a id="ingameManual" href="help.php">
-                    <img class="question" alt="Help" src="/assets/images/x.gif">
+                    <img class="question" alt="Help" src="{{ Vite::asset('resources/images/x.gif') }}">
                 </a>
 
                 <div id="sidebarBeforeContent" class="sidebar beforeContent">
                     @include('partials.hero-side')
                     @include('partials.alliance')
-                    @include('partials.infomsg')
+{{--                    @include('partials.infomsg')--}}
                     @include('partials.links')
                     <div class="clear"></div>
                 </div>
                 <div id="contentOuterContainer">
-                    @include('partials.res')
+{{--                    @include('partials.res')--}}
                     <div class="contentTitle">
-                        <a id="closeContentButton" class="contentTitleButton" href="dorf1.php"></a>
+                        <a id="closeContentButton" class="contentTitleButton" href="/resources"></a>
                     </div>
                     <div class="contentContainer">
                         <div id="content" class="village1">
                             <map name="rx" id="rx">
-                                <?php
-                                $coorarray = array(1 => "180,80,28", "269,81,28", "338,93,28", "122,119,28", "235,132,28", "292,139,28", "377,137,28", "62,170,28", "143,171,28", "333,171,28", "420,171,28", "70,231,28", "143,221,28", "279,257,28", "401,226,28", "174,311,28", "265,316,28", "355,293,28");
-                                for ($i = 1; $i <= 18; $i++) {
-                                $loopsame = $building->isCurrent($i) ? 1 : 0;
-                                if ($loopsame > 0 && $building->isLoop($i)) {
-                                    $doublebuild = 1;
-                                }
-                                $lev = $village->resarray['f' . $i] + ($loopsame > 0 ? 2 : 1) + $doublebuild;
-                                $uprequire = $building->resourceRequired($i, $village->resarray['f' . $i . 't'], ($loopsame > 0 ? 2 : 1) + $doublebuild);
-                                $t = $_SESSION['wid'];
-                                $lvl = $lev - 1;
-                                echo "<area id=\"BuildStatus" . $i . "\" href=\"build.php?id=$i\" coords=\"$coorarray[$i]\" shape=\"circle\" title=\"" . FD_LOADING . "\"/>";
-                                ?>
-
-                                <script type="text/javascript">
-                                    window.addEvent('domready', function () {
-                                        var element = $('BuildStatus<?php echo $i; ?>');
-                                        if (!element) {
-                                            return;
-                                        }
-                                        var fnbuildTitle = function () {
-                                            element.removeEvent('mouseover', fnbuildTitle);
-                                            Travian.ajax({
-                                                data: {
-                                                    cmd: 'getFieldStatus',
-                                                    data: {
-                                                        id: <?php echo $i; ?>
-                                                    }
-                                                },
-                                                onSuccess: function (data) {
-                                                    element.setTitle(data.getFieldStatus);
-                                                }
-                                            });
-                                        };
-                                        element.addEvent('mouseover', fnbuildTitle);
-                                    });
-                                </script>
-                                <?php } ?>
+                                @foreach($buildings as $building)
+                                    <area id="BuildStatus{{ $building['id'] }}" href="{{ url('build', ['id' => $building['id']]) }}" coords="{{ $building['coords'] }}" shape="circle" title="Wait..." />
+{{--                                    <script>--}}
+{{--                                        window.addEvent('domready', function () {--}}
+{{--                                            let element = document.getElementById('BuildStatus{{ $building['id'] }}');--}}
+{{--                                            if (!element) {--}}
+{{--                                                return;--}}
+{{--                                            }--}}
+{{--                                            let fnbuildTitle = function () {--}}
+{{--                                                element.removeEventListener('mouseover', fnbuildTitle);--}}
+{{--                                                Travian.ajax({--}}
+{{--                                                    data: {--}}
+{{--                                                        cmd: 'getFieldStatus',--}}
+{{--                                                        data: {--}}
+{{--                                                            id: {{ $building['id'] }}--}}
+{{--                                                        }--}}
+{{--                                                    },--}}
+{{--                                                    onSuccess: function (data) {--}}
+{{--                                                        element.setTitle(data.getFieldStatus);--}}
+{{--                                                    }--}}
+{{--                                                });--}}
+{{--                                            };--}}
+{{--                                            element.addEventListener('mouseover', fnbuildTitle);--}}
+{{--                                        });--}}
+{{--                                    </script>--}}
+                                @endforeach
                                 <area href="dorf2.php" coords="250,191,32" shape="circle" title="Village Center">
                             </map>
-                            <div id="village_map" class="f<?php echo $village->type; ?>">
+                            <div id="village_map" class="f<?php //echo $village->type; ?>">
                                 <div id="village_circles"></div>
-                                <?php
-                                for ($i = 1; $i <= 18; $i++) {
-                                    if ($village->resarray["f" . $i . "t"] != 0) {
-                                        $text = "";
-                                        switch ($i) {
-                                            case 1:
-                                                $style = "left:180px;top:80px;";
-                                                break;
-                                            case 2:
-                                                $style = "left:269px;top:81px;";
-                                                break;
-                                            case 3:
-                                                $style = "left:338px;top:93px;";
-                                                break;
-                                            case 4:
-                                                $style = "left:122px;top:119px;";
-                                                break;
-                                            case 5:
-                                                $style = "left:235px;top:132px;";
-                                                break;
-                                            case 6:
-                                                $style = "left:292px;top:139px;";
-                                                break;
-                                            case 7:
-                                                $style = "left:377px;top:137px;";
-                                                break;
-                                            case 8:
-                                                $style = "left:62px;top:170px;";
-                                                break;
-                                            case 9:
-                                                $style = "left:143px;top:171px;";
-                                                break;
-                                            case 10:
-                                                $style = "left:333px;top:171px;";
-                                                break;
-                                            case 11:
-                                                $style = "left:420px;top:171px;";
-                                                break;
-                                            case 12:
-                                                $style = "left:70px;top:231px;";
-                                                break;
-                                            case 13:
-                                                $style = "left:143px;top:221px;";
-                                                break;
-                                            case 14:
-                                                $style = "left:279px;top:257px;";
-                                                break;
-                                            case 15:
-                                                $style = "left:401px;top:226px;";
-                                                break;
-                                            case 16:
-                                                $style = "left:174px;top:311px;";
-                                                break;
-                                            case 17:
-                                                $style = "left:265px;top:316px;";
-                                                break;
-                                            case 18:
-                                                $style = "left:355px;top:293px;";
-                                                break;
-                                        }
-                                        $cstyle = "underConstruction";
-                                        if ($village->resarray["f" . $i] != 0) {
-                                            echo "<div class=\"level colorLayer " . ($village->resarray["f" . $i] == 20 ? 'maxLevel' : 'notNow') . " gid" . $village->resarray['f' . $i . 't'] . " level" . $village->resarray['f' . $i] . "\" style=\"" . $style . " \"><div class=\"labelLayer\">" . $village->resarray['f' . $i] . "</div></div>";
-                                        } else {
-                                            echo "<div class=\"level colorLayer good gid" . $village->resarray['f' . $i . 't'] . " level0\" style=\"" . $style . " \"><div class=\"labelLayer\"></div></div>";
-                                        }
-                                        if ($database->getBuildingByField($t, $i)) {
-                                            echo "<div class=\"level colorLayer good " . $cstyle . "\" style=\"" . $style . "\"><div class=\"labelLayer\">" . $village->resarray['f' . $i] . "</div></div>";
-                                        }
-                                    }
-                                }
-                                ?>
-                                <img id="resfeld" usemap="#rx" src="/assets/images/x.gif" alt="">
+{{--                                @foreach ($buildings as $building)--}}
+{{--                                    @php--}}
+{{--                                        $levelClass = $building['level'] != 0 ? 'notNow' : 'good';--}}
+{{--                                        $gidClass = //"gid{$village->resarray['f' . $building['id'] . 't']}";--}}
+{{--                                        $constructionClass = $building['underConstruction'] ? 'underConstruction' : '';--}}
+{{--                                        $classes = "level colorLayer $levelClass $gidClass level{$building['level']} {$building['maxLevelClass']} $constructionClass";--}}
+{{--                                    @endphp--}}
+
+{{--                                    <div class="{{ $classes }}" style="{{ $building['coords'] }}">--}}
+{{--                                        <div class="labelLayer">{{ $building['level'] }}</div>--}}
+{{--                                    </div>--}}
+{{--                                @endforeach--}}
+                                <img id="resfeld" usemap="#rx" src="{{ Vite::asset('resources/images/x.gif') }}" alt="">
                             </div>
                             <div id="map_details">
-                                @include('partials.movement')
+{{--                                @include('partials.movement')--}}
                                 @include('partials.production')
                                 @include('partials.troops')
                                 <div class="clear"></div>
@@ -154,9 +82,9 @@
                     </div>
                 </div>
                 <div id="sidebarAfterContent" class="sidebar afterContent">
-                    @include('partials.sideinfo')
+{{--                    @include('partials.sideinfo')--}}
                     @include('partials.multivillage')
-                    @include('partials.quest')
+{{--                    @include('partials.quest')--}}
                 </div>
                 <div class="clear"></div>
             </div>
@@ -165,40 +93,19 @@
         <div id="ce"></div>
     </div>
 
+
+    <script type="text/javascript">
+        window.addEvent('domready', function () {
+            Travian.Game.Layout.goldButtonAnimation();
+        });
+    </script>
+    <script type="text/javascript">
+        $$('#outOfGame li.logout a').addEvent('click', function () {
+            Travian.WindowManager.getWindows().each(function ($dialog) {
+                Travian.WindowManager.unregister($dialog);
+            });
+        });
+    </script>
+
 @endsection
 
-<script type="text/javascript">
-    window.addEvent('domready', function () {
-        Travian.Game.Layout.goldButtonAnimation();
-    });
-</script>
-<script type="text/javascript">
-    $$('#outOfGame li.logout a').addEvent('click', function () {
-        Travian.WindowManager.getWindows().each(function ($dialog) {
-            Travian.WindowManager.unregister($dialog);
-        });
-    });
-</script>
-<script type="text/javascript">
-    window.addEvent('domready', function () {
-        var element = $('BuildStatus<?php echo $i; ?>');
-        if (!element) {
-            return;
-        }
-        var fnbuildTitle = function () {
-            element.removeEvent('mouseover', fnbuildTitle);
-            Travian.ajax({
-                data: {
-                    cmd: 'getFieldStatus',
-                    data: {
-                        id: <?php echo $i; ?>
-                    }
-                },
-                onSuccess: function (data) {
-                    element.setTitle(data.getFieldStatus);
-                }
-            });
-        };
-        element.addEvent('mouseover', fnbuildTitle);
-    });
-</script>
