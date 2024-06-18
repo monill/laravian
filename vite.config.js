ecf6633840f81ff4358f6ab59b4853d407fdde3f
John <juaorok@hotmail.com>
2024-06-18 14:48:47 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,14 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // CSS
                 'resources/css/app.css',
                 'resources/css/activate.css',
+                'resources/css/compact.css',
+                'resources/css/compact1.css',
+                'resources/css/chat.css',
+                'resources/css/lang.css',
+                // JS
                 'resources/js/app.js',
                 'resources/js/activate.js',
             ],
