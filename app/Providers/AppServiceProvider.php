22f5760112724d379f3a866a1e2b71d61a5ef2da
John <juaorok@hotmail.com>
2024-06-18 14:51:36 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\View\Components\AllianceLayout;
+use App\View\Components\HeroSideLayout;
+use App\View\Components\LinksLayout;
+use App\View\Components\TroopsLayout;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        view()->composer('partials.hero-side', function ($view) {
+            $view->with('hero', HeroSideLayout::hero());
+        });
+
+        view()->composer('partials.alliance', function ($view) {
+            $view->with('allianceInfo', AllianceLayout::alliance());
+        });
+
+        view()->composer('partials.links', function ($view) {
+            $view->with('links', LinksLayout::links());
+        });
+
+        view()->composer('partials.links', function ($view) {
+            $view->with('timestamp', LinksLayout::timestamp());
+        });
+
+        view()->composer('partials.troops', function ($view) {
+            $view->with('units', TroopsLayout::showTroops());
+        });
     }
 }
