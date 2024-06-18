79a4efb9d177e670803921a2da30d22a0d428a86
John <juaorok@hotmail.com>
2024-06-18 14:49:29 -0300

    Update production.blade.php

--- resources/views/partials/production.blade.php
+++ resources/views/partials/production.blade.php
@@ -11,54 +11,64 @@
     <div class="boxes-contents cf">
         <table id="production" cellpadding="1" cellspacing="1">
             <thead>
-            <tr>
-                <th colspan="4"><?php echo PD_PRODPERHR; ?></th>
-            </tr>
+                <tr>
+                    <th colspan="4">Products per hour:</th>
+                </tr>
             </thead>
             <tbody>
-            <tr>
-                <td class="ico">
-                    <div><img class="r1Big" src="/assets/images/x.gif" alt="<?php echo PD_LUMBER; ?>" title="<?php echo PD_LUMBER; ?>" />
-                        <?php if ($golds['b1']) echo '<img src="/assets/images/x.gif" class="productionBoost" alt="' . PD_WOODBONUS . '">'; ?>
-                    </div>
-                </td>
-                <td class="res"><?php echo PD_LUMBER; ?>:</td>
-                <td class="num"><?php echo round($village->getProd('wood')); ?></td>
-            </tr>
-            <tr>
-                <td class="ico">
-                    <div><img class="r2Big" src="/assets/images/x.gif" alt="<?php echo PD_CLAY; ?>" title="<?php echo PD_CLAY; ?>" />
-                        <?php if ($golds['b2']) echo '<img src="/assets/images/x.gif" class="productionBoost" alt="' . PD_CLAYBONUS . '">'; ?>
-                    </div>
-                </td>
-                <td class="res"><?php echo PD_CLAY; ?>:</td>
-                <td class="num"><?php echo round($village->getProd('clay')); ?></td>
-            </tr>
-            <tr>
-                <td class="ico">
-                    <div><img class="r3Big" src="/assets/images/x.gif" alt="<?php echo PD_IRON; ?>" title="<?php echo PD_IRON; ?>" />
-                        <?php if ($golds['b3']) echo '<img src="/assets/images/x.gif" class="productionBoost" alt="' . PD_IRONBONUS . '">'; ?>
-                    </div>
-                </td>
-                <td class="res"><?php echo PD_IRON; ?>:</td>
-                <td class="num"><?php echo round($village->getProd('iron')); ?></td>
-            </tr>
-            <tr>
-                <td class="ico">
-                    <div><img class="r4Big" src="/assets/images/x.gif" alt="<?php echo PD_CROP; ?>" title="<?php echo PD_CROP; ?>" />
-                        <?php if ($golds['b4']) echo '<img src="/assets/images/x.gif" class="productionBoost" alt="' . PD_CROPBONUS . '">'; ?>
-                    </div>
-                </td>
-                <td class="res"><?php echo PD_CROP; ?>:</td>
-                <td class="num">
-                    <?php echo $village->getProd("crop"); ?>
-                </td>
-            </tr>
+                <tr>
+                    <td class="ico">
+                        <div>
+                            <img class="r1Big" src="{{ Vite::asset('resources/images/x.gif') }}" alt="Wood" title="Wood" />
+                            @if(auth()->user()->b1)
+                                <img src="{{ Vite::asset('resources/images/x.gif') }}" class="productionBoost" alt="Increase wood production">
+                            @endif
+                        </div>
+                    </td>
+                    <td class="res">Wood:</td>
+{{--                    <td class="num"><?= $village->getProd('wood'); ?></td>--}}
+                </tr>
+                <tr>
+                    <td class="ico">
+                        <div>
+                            <img class="r2Big" src="{{ Vite::asset('resources/images/x.gif') }}" alt="Clay" title="Clay" />
+                            @if(auth()->user()->b2)
+                                <img src="{{ Vite::asset('resources/images/x.gif') }}" class="productionBoost" alt="Increased clay production">
+                            @endif
+                        </div>
+                    </td>
+                    <td class="res">Clay:</td>
+{{--                    <td class="num"><?= $village->getProd('clay'); ?></td>--}}
+                </tr>
+                <tr>
+                    <td class="ico">
+                        <div>
+                            <img class="r3Big" src="{{ Vite::asset('resources/images/x.gif') }}" alt="Iron" title="Iron" />
+                            @if(auth()->user()->b3)
+                                <img src="{{ Vite::asset('resources/images/x.gif') }}" class="productionBoost" alt="Increase iron production">
+                            @endif
+                        </div>
+                    </td>
+                    <td class="res">Iron:</td>
+{{--                    <td class="num"><?= $village->getProd('iron'); ?></td>--}}
+                </tr>
+                <tr>
+                    <td class="ico">
+                        <div>
+                            <img class="r4Big" src="{{ Vite::asset('resources/images/x.gif') }}" alt="Crop" title="Crop" />
+                            @if(auth()->user()->b4)
+                                <img src="{{ Vite::asset('resources/images/x.gif') }}" class="productionBoost" alt="Increase crop production">
+                            @endif
+                        </div>
+                    </td>
+                    <td class="res">Crop:</td>
+{{--                    <td class="num"><?= $village->getProd("crop"); ?></td>--}}
+                </tr>
             </tbody>
         </table>
 
         <div>
-            <button type="button" value="&nbsp;25%&nbsp;" id="button5229e52541034" class="gold productionBoostButton" title="<?php echo PD_MINFO; ?>">
+            <button type="button" value="&nbsp;25%&nbsp;" id="button5229e52541034" class="gold productionBoostButton" title="Learn more about increasing productivity">
                 <div class="button-container addHoverClick">
                     <div class="button-background">
                         <div class="buttonStart">
@@ -76,11 +86,11 @@
                         $('button5229e52541034').addEvent('click', function() {
                             window.fireEvent('buttonClicked', [this, {
                                 "type": "button",
-                                "value": "<?php echo PD_MINFO; ?>",
+                                "value": "Learn more about increasing productivity",
                                 "name": "",
                                 "id": "button5229e52541034",
                                 "class": "gold productionBoostButton",
-                                "title": "<?php echo PD_MINFO; ?>",
+                                "title": "Learn more about increasing productivity",
                                 "confirm": "",
                                 "onclick": "",
                                 "productionBoostDialog": {
